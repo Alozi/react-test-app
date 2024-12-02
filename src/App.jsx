466bee4f69bf679278216c6ecea39b45d7f4e657
Alozi <alozimozi@gmail.com>
2024-12-02 17:16:25 +0200

    Create React App

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Victoria Bogustka React Test App{" "}
+          <a
+            className="App-link"
+            href="https://dummy-api.d0.acom.cloud/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            https://dummy-api.d0.acom.cloud/
+          </a>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
