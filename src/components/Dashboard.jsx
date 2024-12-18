05cf07df4cf72220a235f824de59f25b87119842
Alozi <alozimozi@gmail.com>
2024-12-18 14:45:53 +0200

    Add ProductCard component

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,8 +1,15 @@
+import ProductCard from "./ProductCard";
+
 export default function Dashboard() {
   return (
-    <>
+    <div className="dashboard">
       <h2>Dashboard</h2>
       <p>TEST TEXT</p>
-    </>
+      <div className="wrapper">
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+      </div>
+    </div>
   );
 }
---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,17 @@
+export default function ProductCard() {
+  return (
+    <div className="product-item">
+      <img
+        src="http://dummy-api.d0.acom.cloud/assets/image.jpg"
+        alt="voluptates"
+      />
+      <h3>voluptates</h3>
+      <p>
+        Ipsum nihil odit tempore ea quis corrupti. Voluptatum explicabo et
+        voluptatem sed veritatis et. Harum nesciunt non rem expedita aperiam
+        magni numquam. Officiis quas debitis in aspernatur corrupti quo.
+      </p>
+      <span>6049.00</span>
+    </div>
+  );
+}
